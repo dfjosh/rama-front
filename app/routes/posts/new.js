aafa953f7a9cb10e69c3d98715b7aad5afe6e003
Joshua Wetenkamp <dfjosh@me.com>
2016-08-25 00:02:02 -0700

    two models, dropdowns working

--- app/routes/posts/new.js
+++ app/routes/posts/new.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
+import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
+  model() {
+    return RSVP.hash({
+      categories: this.store.findAll('category'),
+      tags:       this.store.findAll('tag')
+    });
+  }
 });
