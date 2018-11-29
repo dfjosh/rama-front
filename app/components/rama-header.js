31b763aee3d60ceda7c792053551f68b963e426b
Joshua Wetenkamp <dfjosh@me.com>
2018-11-28 19:48:22 -0800

    componentized header and nav, cleaned up admin toolbar

--- app/components/rama-header.js
+++ app/components/rama-header.js
@@ -1,4 +1,8 @@
 import Component from '@ember/component';
+import ENV from '../config/environment';
 
 export default Component.extend({
+  classNames: ['rama-header'],
+  tagName: 'header',
+  ENV: ENV
 });
--- tests/integration/components/rama-nav-test.js
+++ tests/integration/components/rama-nav-test.js
@@ -1,18 +1,18 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, find } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | rama nav', function(hooks) {
+module('Integration | Component | rama-nav', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.on('myAction', function(val) { ... });
+    // Handle any actions with this.set('myAction', function(val) { ... });
 
     await render(hbs`{{rama-nav}}`);
 
-    assert.dom('*').hasText('');
+    assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
@@ -21,6 +21,6 @@ module('Integration | Component | rama nav', function(hooks) {
       {{/rama-nav}}
     `);
 
-    assert.dom('*').hasText('template block text');
+    assert.equal(this.element.textContent.trim(), 'template block text');
   });
 });
