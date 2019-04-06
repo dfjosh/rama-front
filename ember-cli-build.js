71540506d895955c4f1b7ef5b0c89a270b5a6de0
Joshua Wetenkamp <dfjosh@me.com>
2019-04-06 11:10:51 -0700

    deploy stuff

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    fingerprint: {
+      prepend: 'https://lazy-rama.s3.amazonaws.com/rama-front/'
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
