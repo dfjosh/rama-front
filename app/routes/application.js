d994cd3b33ee948d6c09cf35efac49ed02338ee1
Joshua Wetenkamp <dfjosh@me.com>
2019-04-12 23:20:50 -0700

    current.user working!

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,22 @@
 import Route from '@ember/routing/route';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
- 
-export default Route.extend(ApplicationRouteMixin);
+import { inject as service } from '@ember/service';
+
+export default Route.extend(ApplicationRouteMixin, {
+  current: service(),
+  
+  beforeModel() {
+    return this._loadCurrentUser();
+  },
+  
+  sessionAuthenticated() {
+    this._super(...arguments);
+    this._loadCurrentUser();
+  },
+  
+  _loadCurrentUser() {
+    return this.current.load().catch(() => {
+      this.session.invalidate();
+    });
+  }
+});
