bf1cd3397d4a41e7315a803bbbafb60e4f4a8e00
Joshua Wetenkamp <dfjosh@me.com>
2020-02-26 22:20:02 -0800

    csp

--- config/environment.js
+++ config/environment.js
@@ -8,7 +8,7 @@ module.exports = function(environment) {
     locationType: 'router-scroll', // 'auto'
     historySupportMiddleware: true,
     contentSecurityPolicy: {
-      'font-src': "'self' https://www.distantfuturejosh.com"
+      'font-src': "'self' https://lazy-rama.s3.amazonaws.com"
     },
     EmberENV: {
       FEATURES: {
